774df3ada500a440ff8d00e427a8414ce42e5be8
Guilherme Samuel de Souza Barbosa <gui.samuel10@gmail.com>
2022-11-06 13:01:57 -0300

    adicionado CORS no barramento

--- backend/barramento/index.js
+++ backend/barramento/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const axios = require('axios');
 
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
+app.use(express.json());
+
 
 const eventos = [];
 
